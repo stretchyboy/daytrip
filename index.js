28efdcd1d94c84121c7a70d9f157c56e41260025
Martyn Eggleton <martyn.eggleton@gmail.com>
2016-08-06 14:05:07 +0100

    Make your own directories :)

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var fs = require("fs");
 var httpAdapter = 'http';
 var moment = require("moment");
 
-var oEH = JSON.parse(fs.readFileSync("eh.json").toString());
-
+//File from import.io screen scraper
+var FileName = "English Heritage Properties.json";
+var oEH = JSON.parse(fs.readFileSync(FileName).toString());
 
 var ArgumentParser = require('argparse').ArgumentParser;
 
@@ -13,116 +14,66 @@ var parser = new ArgumentParser({
   description: 'Day trip lister'
 });
 
-parser.addArgument(
-  [ '-p', '--postcode' ],
-  {
-    help: "Post Code",
-    defaultValue: "S6 5QL",
-    dest:"sPostcode"
-  }
-);
-
-
-parser.addArgument(
-  [ '-r', '--ruins' ],
-  {
-    help: "Include Ruins",
-    action: 'storeTrue',
-    defaultValue: false,
-    dest:"bIncludeRuins"
-  }
-);
-
-parser.addArgument(
-  [ '-m', '--maxmiles' ],
-  {
-    help: "Maximum Miles",
-    defaultValue: 100,
-    dest:"iMaxMiles"
-  }
-);
-
-parser.addArgument(
-  [ '-t', '--maxtimes' ],
-  {
-    help: "Maximum Time in Minutes",
-    defaultValue: 180,
-    dest:"iMaxTime"
-  }
-);
-
-parser.addArgument(
-  [ '-c', '--maxcalls' ],
-  {
-    help: "Maximum number of Calls in a run",
-    defaultValue: 180,
-    dest:"iMaxCalls"
-  }
-);
-
-
-parser.addArgument(
-  [ '-g', '--geocoder' ],
-  {
-    help: "Geocoder Provider",
-    defaultValue: "google",
-    dest:"geocoderProvider"
-  }
-);
-
-parser.addArgument(
-  [ '-j', '--journeycall' ],
-  {
-    help: "Make the Journey planning calls",
-    action: 'storeTrue',
-    defaultValue: false,
-    dest:"bJourneyCalls"
-  }
-);
-
-
-parser.addArgument(
-  [ '-d', '--debug' ],
-  {
-    help: "Turn debugging on",
-    action: 'storeTrue',
-    defaultValue: false,
-    dest:"bDebug"
-  }
-);
+parser.addArgument(  [ '-p', '--postcode' ],
+  { help: "Post Code to search around", defaultValue: "S6 5QL" });
+parser.addArgument(  [ '--ruins' ],
+  { help: "Include Ruins in search results", action: 'storeTrue', defaultValue: false });
+parser.addArgument(  [ '-r', '--radius' ],
+  { help: "Maximum as crow flies distance in Miles", defaultValue: 50 });
+parser.addArgument(  [ '-j', '--journeycalls' ],
+  { help: "Make any Journey planning calls", action: 'storeTrue', defaultValue: true});
+parser.addArgument(  [ '-t', '--maxtime' ],
+  { help: "Maximum Journey Time in Minutes", defaultValue: 120});
+parser.addArgument(  [ '-l', '--maxlcalls' ],
+  { help: "Maximum number of Location Calls in a run", defaultValue: 10});
+parser.addArgument(  [ '-x', '--maxjcalls' ],
+  { help: "Maximum number of Journey Calls in a run", defaultValue: 50});
+parser.addArgument(  [ '-a', '--tapi' ],
+  { help: "Transport API Credentials to use", defaultValue: "default"});
+parser.addArgument(  [ '-g', '--geocoder' ],
+  { help: "Geocoder Provider (google / openstreetmap)", defaultValue: "google"});
+parser.addArgument(  [ '-s', '--summary' ],
+  { help: "Show summary of the Routes found", action: 'storeTrue', defaultValue: false });
+parser.addArgument(  [ '-d','--debug' ],
+  { help: "Turn debugging on", action: 'storeTrue', defaultValue: false});
 
 var options = parser.parseArgs();
-//console.log("options =", options);
 
-var geocoderProvider = options.geocoderProvider;//'google';
+var geocoderProvider = options.geocoder;//'google';
 var geocoder = require('node-geocoder')(geocoderProvider, httpAdapter);//, extra);
 
-var sPostcode = options.sPostcode;//"S6 5QL";
-var iMaxMiles = options.iMaxMiles;//100;
-var iMaxTime = options.iMaxTime;//180;
-var iMaxCalls = options.iMaxCalls;//10;
+function safeBaseName(Path){
+  return Path.replace(/[\/\:\+\,\s\t]/g,"_");
+}
 
-var bJourneyCalls = options.bJourneyCalls;//false;
-var bDebug = options.bDebug;//false;
-
-var sCantLocateFile = "data/cant_locate_"+geocoderProvider+".json";
-var sPostcodeJSON = "data/"+sPostcode+".json";
+var sCantLocateFile = ("data/cant_locate_"+geocoderProvider+".json");
+var sLocationsFile = ("data/locations.json");
+var sPostcodeJSON = ("data/"+safeBaseName(options.postcode)+".json");
 var iCalls = 0;
+var iJCalls = 0;        
 
 var aCantLocate = [];
 if(fs.existsSync(sCantLocateFile)) {
   aCantLocate = JSON.parse(fs.readFileSync(sCantLocateFile).toString());
 }
-//console.log("aCantLocate =", aCantLocate);
+
+var oLocations = {};
+if(fs.existsSync(sLocationsFile)) {
+  oLocations = JSON.parse(fs.readFileSync(sLocationsFile).toString());
+}
 
 var aTooFar = [];
-var sTooFarFile = "data/too_far_"+sPostcode+".json";
-if(fs.existsSync(sTooFarFile)) {
+// TODO : options.postcode.replace(" ", "_")+"_"+options.radius
+var sTooFarFile = ("data/too_far_"+safeBaseName(options.postcode)+".json");
+/*if(fs.existsSync(sTooFarFile)) {
   aTooFar = JSON.parse(fs.readFileSync(sTooFarFile).toString());
-}
-//console.log("aTooFar =", aTooFar);
+}*/
 
-var oTransportAPI = JSON.parse(fs.readFileSync("transportapi.json").toString());
+var oTransportAPIAll = JSON.parse(fs.readFileSync("transportapi.json").toString());
+var oTransportAPI = oTransportAPIAll[options.tapi];
+
+var sURLBase = "http://transportapi.com/v3/uk/public/journey/";
+var sAppCredintials = ".json?region=southeast&api_key="+oTransportAPI.api_key+"&app_id="+oTransportAPI.app_id;
 
 
 function distance(lat1, lon1, lat2, lon2, unit) {
@@ -141,19 +92,21 @@ function distance(lat1, lon1, lat2, lon2, unit) {
 	    return dist;
 	}
 
+//Get the starting point Postcode data (inc lon/lat)
 var oPostCode;
-
 if(fs.existsSync(sPostcodeJSON)) {
   oPostCode = JSON.parse(fs.readFileSync(sPostcodeJSON).toString());
 } else {
-  geocoder.geocode(sPostcode, function(err, oPostCodeRes) {
+  geocoder.geocode(options.postcode, function(err, oPostCodeRes) {
       oPostCode = oPostCodeRes[0] ;//.longitude+","+oPostCodeRes[0].latitude;
       fs.writeFileSync(sPostcodeJSON, JSON.stringify(oPostCode, null, 4)); 
   });
 }
 
+
+//Spread the Region Names across the Properties listed after the region subheading
 var sRegion = "";
-var aProperties = oEH.results.map(function(oProperty){
+var aProperties = oEH.map(function(oProperty){
    if (oProperty.region && !oProperty["name/_text"]) {
      sRegion = oProperty.region;
    } else {
@@ -164,6 +117,7 @@ var aProperties = oEH.results.map(function(oProperty){
    return oProperty;
 });
 
+//Filter out the Region subheadings
 aProperties = aProperties.filter(function(oProperty){
    if (oProperty.region && !oProperty["name/_text"]) {
      return false;
@@ -171,131 +125,186 @@ aProperties = aProperties.filter(function(oProperty){
    return true;
 });
 
-
-
 aProperties = aProperties.filter(function(oProperty){
-   if (!options.bIncludeRuins && typeof oProperty.condition !== "undefined") {
+   // Filter for ruins 
+   if (!options.includeruins && typeof oProperty.condition !== "undefined") {
       if(oProperty.condition === "Ruins") {
         return false;
       }
    }
-   
+   //Filter out the Properties we can't Locate
    if(aCantLocate.indexOf(oProperty.search) !== -1){
      return false;
    }
-   
+   //Filter out the Properties we that are Too Far
    if(aTooFar.indexOf(oProperty.search) !== -1){
      return false;
    }
-   
    return true;
 });
 
-console.log("aProperties =", aProperties.length);
-//aProperties = aProperties.slice(0,15); 
-//if (bDebug) {console.log("aProperties =", aProperties);}
+//console.log("Located properties that are within "+options.radius+" miles =", aProperties.length);
 
-
+if(options.includeruins){
+  console.log("Including ruins");
+}
 
 var sDateFrag = "/by/"+moment().endOf('week').add(2, "day").hour(12).minute(0).format("YYYY-MM-DD/hh:mm");
 
 // Using callback 
 aProperties.forEach(function(oProperty) {
-  if (bDebug) {console.log("Looking for Property", oProperty.search);}
-  var sFileName = "journey_plans/"+(sPostcode).replace(/[\/\:\+\,]/g,"_")+"_"+oProperty.filefrag+".json"; 
+  if (options.debug) {console.log("Looking for Property", oProperty.search);}
+  var sFileName = ("journey_plans/"+safeBaseName(options.postcode)+"_"+oProperty.filefrag+".json"); 
+  
+  //If we have a relevant journey plan
   if(fs.existsSync(sFileName)) {
       var oResponce = JSON.parse(fs.readFileSync(sFileName).toString());
-      if (bDebug) {console.log("From File "+sFileName);}
-      displayJourney(oProperty, false, oResponce);
-  } else if (iCalls < iMaxCalls) {    
-    iCalls ++;
+      if (options.debug) {console.log("From File "+sFileName);}
+      displayJourney(oProperty, false, oResponce, options.postcode);
+  } else {
     
-    geocoder.geocode(oProperty.search.replace("&", " "), function(err, res) {
-      
-      //Do the journey plan
-      var sURLBase = "http://transportapi.com/v3/uk/public/journey/";
-      var sAppCredintials = ".json?region=southeast&api_key="+oTransportAPI.api_key+"&app_id="+oTransportAPI.app_id;
-      if(res.length === 0) {
-        console.log("Can't Find Geo Location for Property", oProperty.search);
-        
-        aCantLocate.push(oProperty.search);
-        //console.log("aCantLocate =", aCantLocate);
-        fs.writeFileSync(sCantLocateFile, JSON.stringify(aCantLocate, null, 4));
-      } else {
-        if(typeof res.raw !== "undefined" && typeof res.raw.error_message  !== "undefined") {
-          console.log(res.raw.error_message);
-        } else {
-          var iDist = distance(oPostCode.latitude, oPostCode.longitude, res[0].latitude, res[0].longitude, "M");
-          
-          //console.log("iDist =", iDist);
-          if(iDist < iMaxMiles) {
-            if(bJourneyCalls) {
-              console.log("Looking up Journey to ", oProperty.search);
-              var sFrom = "from/postcode:"+sPostcode.replace("", "+");
-              var sTo = "/to/lonlat:"+res[0].longitude+","+res[0].latitude;
-              var sURL = sURLBase+sFrom+sTo+sDateFrag+sAppCredintials;
-              if (bDebug) {console.log("sURL =", sURL);}
-              if (bDebug) {console.log("Saving to File "+sFileName);}
-              var http = require('http');
-              http.get(sURL,  function(response) {
-                var str = '';
-                if(response.statusCode === 200) {
-                  //another chunk of data has been recieved, so append it to `str`
-                  response.on('data', function (chunk) {
-                    str += chunk;
-                  });
-                
-                  //the whole response has been recieved, so we just print it out here
-                  response.on('end', function () {
-                    var oResponce = JSON.parse(str); 
-                    if(typeof oResponce.error !== "undefined") {
-                      console.log("Error ", oResponce.error);
-                    } else {
-                      fs.writeFileSync(sFileName, str);
-                      console.log("Saved to ",sFileName);
-                      displayJourney(oProperty, res[0], oResponce);
-                    }
-                  });
-                } else {
-                  console.log("response.statusCode =", response.statusCode);
-                }
-                
-                response.on('error', function(e) {
-                  console.log("Got error: " + e.message);
-                });
-              });
-            } else {
-              //iCalls --; //not sure this will work
-            }
+    if(oLocations.hasOwnProperty(oProperty.search)) {
+      onLocation(oLocations[oProperty.search], oProperty);
+    } else if (iCalls < options.maxlcalls) {     //Do we have to locate this Property?
+      iCalls ++;
+      // TODO : Map the distances / sort / filter
+      // TODO : Geocoder call throttling
+      geocoder.geocode(oProperty.search.replace("&", " "), function(err, res) {      
+        //Do the journey plan
+        if(res.length === 0) {
+          console.log("Can't Find Geo Location for Property", oProperty.search);
+          aCantLocate.push(oProperty.search);
+          fs.writeFileSync(sCantLocateFile, JSON.stringify(aCantLocate, null, 4));
+        } else { // Got a responce
+          if(typeof res.raw !== "undefined" && typeof res.raw.error_message  !== "undefined") {
+            console.log("GeoCoder : "+res.raw.error_message );
+            console.log("After "+ iCalls +" Calls");
+          } else if (typeof oPostCode !== "undefined"){ // We have a Location and a start position
+            // TODO : save res[0] to oLocations
+            oLocations[oProperty.search] = res[0];
+            fs.writeFileSync(sLocationsFile, JSON.stringify(oLocations, null, 4));
+            onLocation(res[0], oProperty);
           } else {
-            aTooFar.push(oProperty.search);
-            console.log(oProperty.search+" is too far at ", iDist);
-            //console.log("aTooFar =", aTooFar);
-            fs.writeFileSync(sTooFarFile, JSON.stringify(aTooFar, null, 4));
-            //iCalls --; //not sure this will work
+            console.log("Postcode "+" not found");
           }
         }
-      }
-    });
+      });
+    }
   }
 });
 
+function onLocation(oLocation, oProperty){
+  var iDist = distance(oPostCode.latitude, oPostCode.longitude, oLocation.latitude, oLocation.longitude, "M");
+            
+  //console.log("iDist =", iDist);
+  if(iDist < options.radius) {
+    if(options.journeycalls && iJCalls < options.maxjcalls) {
+      iJCalls ++;
+      console.log("Looking up Journey to ", oProperty.search);
+      var sFrom = "from/postcode:"+options.postcode.replace("", "+");
+      var sTo = "/to/lonlat:"+ oLocation.longitude+","+ oLocation.latitude;
+      var sURL = sURLBase+sFrom+sTo+sDateFrag+sAppCredintials;
+      if (options.debug) {console.log("sURL =", sURL);}
+      if (options.debug) {console.log("Saving to File "+sFileName);}
+      var http = require('http');
+      http.get(sURL,  function(response) {
+        var str = '';
+        if(response.statusCode === 200) {
+          //another chunk of data has been recieved, so append it to `str`
+          response.on('data', function (chunk) {
+            str += chunk;
+          });
+        
+          //the whole response has been recieved, so we just print it out here
+          response.on('end', function () {
+            var oResponce = JSON.parse(str); 
+            if(typeof oResponce.error !== "undefined") {
+              console.log("Error ", oResponce.error);
+            } else {
+              
+              var sFileName = ("journey_plans/"+safeBaseName(options.postcode)+"_"+oProperty.filefrag+".json"); 
+              fs.writeFileSync(sFileName, JSON.stringify(oResponce, null, 4));
+              console.log("Saved to ",sFileName);
+              displayJourney(oProperty,  oLocation, oResponce, options.postcode);
+            }
+          });
+        } else {
+          console.log("response.statusCode =", response.statusCode);
+        }
+        
+        response.on('error', function(e) {
+          console.log("Got error: " + e.message);
+        });
+      });
+    }
+  }/* else {
+    aTooFar.push(oProperty.search);
+    console.log(oProperty.search+" is too far at ", Math.round(iDist)+ " miles");
+    //console.log("aTooFar =", aTooFar);
+    fs.writeFileSync(sTooFarFile, JSON.stringify(aTooFar, null, 4));
+  }*/
+}
 
-function displayJourney(oProperty, oLocation, oJourney){
+function displayJourney(oProperty, oLocation, oJourney, sPostcode){
   if(oJourney.routes.length > 0) {
     var aDurations = oJourney.routes.filter(function(oRoute){
         return oRoute.route_parts[0].mode !== "taxi";
-    }).map(function(oRoute){
+        }).map(function(oRoute){
         return oRoute.duration;
     });
+    
+    
     var aSorted = aDurations.sort();
     var aDur = aSorted[0].split(":");
     var iMins = (parseInt(aDur[0])*60) + parseInt(aDur[1]);
-    if(iMins < iMaxTime) {
+    if(iMins < options.maxtime) {
+      
+      var aNoTaxis = oJourney.routes.filter(function(oRoute){
+        return oRoute.route_parts.every(function(oPart){
+          return oPart.mode !== "taxi";
+        });
+      });
+      
+      var aTaxiDurations = oJourney.routes.map(function(oRoute){
+        return oRoute.route_parts.reduce(function(previousValue , oPart){
+          return previousValue + (oPart.mode == "taxi")?oPart.duration:0;
+        });
+      });
+      
+      /*if(aNoTaxis.length == 0){
+        console.log("Only has Journies including Taxis");
+        console.log(aTaxiDurations.join(",  "));
+      }*/
+
       console.log(oProperty.search+ " ("+oProperty["type/_text"]+") takes " + aDurations[0]+ "-"+aDurations[aDurations.length - 1]);
+      if(options.summary) {
+        var sRoutesSummary = oJourney.routes.filter(function(oRoute){
+          return oRoute.route_parts[0].mode !== "taxi";
+        }).map(function(oRoute){
+          return " "+oRoute.departure_time+"("+oRoute.duration.replace(/\:00$/,"")+") "+oRoute.route_parts.map(function(oPart){
+            return oPart.mode+"("+oPart.duration.replace(/\:00$/,"")+")";
+          }).join(", ");
+        }).join("\n");
+      
+        console.log(sRoutesSummary);
+      }
+      
+      var aRoutes = oJourney.routes.filter(function(oRoute){
+        return oRoute.route_parts[0].mode !== "taxi";
+      }).map(function(oRoute){
+        oRoute.route_parts = oRoute.route_parts.map(function(oPart){
+          delete oPart.coordinates;
+          return oPart;
+        });
+        return oRoute;
+      });
+      var oSummary = new Object(oJourney);
+      oSummary.routes = aRoutes;
+      var sFileName = "journey_summary/"+(sPostcode).replace(/[\/\:\+\,]/g,"_")+"_"+oProperty.filefrag+".json";
+      fs.writeFileSync(sFileName, JSON.stringify(oSummary, null, 4));
     }
   } else {
-    if(bDebug) {console.log("No Journey to "+oProperty.search);}
+    if(options.debug) {console.log("No Journey to "+oProperty.search);}
   }
 }
   
